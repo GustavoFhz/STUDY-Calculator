ba521de0372915aa50b7146b1eab6bc651640f66
GustavoFhz <gustavojesus79@gmail.com>
2024-07-25 00:09:43 -0300

    Game realizado com a divisão das operações matemáticas na classe MathOperations

---
+++ Calculator/MathOperations.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class MathOperations
+    {
+        public static void Addition()
+        {
+
+            Random random = new Random();
+            var score = 0;
+
+            for (int i = 0; i <= 5; i++)
+            {
+                var num1 = random.Next(1, 10);
+                var num2 = random.Next(1, 10);
+                Console.WriteLine($"{num1} + {num2} = ");
+                var result = int.Parse(Console.ReadLine());
+
+
+                if (result == num1 + num2)
+                {
+                    Console.WriteLine("You're correct");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("You're wrong");
+                }
+            }
+            Console.WriteLine($"You scored {score} points");
+
+            Program.AddGame($"{DateTime.Now} - Addition: Score = {score}");
+
+            Console.WriteLine("Press any key to play again");
+            Console.ReadLine();
+
+            
+        }
+        public static void Subtraction()
+        {
+            Random random = new Random();
+            var score = 0;
+
+            for (int i = 0; i <= 5; i++)
+            {
+                var num1 = random.Next(1, 10);
+                var num2 = random.Next(1, 10);
+                Console.WriteLine($"{num1} - {num2} = ");
+                var result = int.Parse(Console.ReadLine());
+
+
+                if (result == num1 - num2)
+                {
+                    Console.WriteLine("You're correct");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("You're wrong");
+                }
+            }
+            Console.WriteLine($"You scored {score} points");
+
+            Program.AddGame($"{DateTime.Now} - Subtraction: Score = {score}");
+
+            Console.WriteLine("Press any key to to play again");
+            Console.ReadLine();
+
+        }
+        public static void Multiplication()
+        {
+            Random random = new Random();
+            var score = 0;
+
+            for (int i = 0; i <= 5; i++)
+            {
+                var num1 = random.Next(1, 10);
+                var num2 = random.Next(1, 10);
+                Console.WriteLine($"{num1} * {num2} = ");
+                var result = int.Parse(Console.ReadLine());
+
+
+                if (result == num1 * num2)
+                {
+                    Console.WriteLine("You're correct");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("You're wrong");
+                }
+            }
+            Console.WriteLine($"You scored {score} points");
+
+            Program.AddGame($"{DateTime.Now} - Multiplication: Score = {score}");
+
+            Console.WriteLine("Press any key to to play again");
+            Console.ReadLine();
+
+        }
+        public static void Division()
+        {
+            Random random = new Random();
+            var score = 0;
+
+            for (int i = 0; i <= 5; i++)
+            {
+                var num1 = random.Next(1, 10);
+                var num2 = random.Next(1, 10);
+
+                while (num1 % num2 != 0)
+                {
+                    num1 = random.Next(1, 10);
+                    num2 = random.Next(1, 10);
+                }
+                Console.WriteLine($"{num1} / {num2} = ");
+                int result = int.Parse(Console.ReadLine());
+
+                if (result == num1 / num2)
+                {
+                    Console.WriteLine("You're correct");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("You're wrong");
+                }
+            }
+            Console.WriteLine($"You scored {score} points");
+
+            Program.AddGame($"{DateTime.Now} - Division: Score = {score}");
+
+            Console.WriteLine("Press any key to play again");
+            Console.ReadLine();
+
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,9 +2,78 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+       static List<string> games = new();
+        
+       static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Menu();
+            
+        }
+       static void Menu()
+        {
+            var gameOn = true;
+            do
+            {
+                Console.WriteLine("Welcome to the MathGame, please type your name:");
+                var name = Console.ReadLine();
+                var date = DateTime.Now;
+                Console.Clear();
+
+                Console.WriteLine($"Hello {name}, It's {date}. Please select the option you want to play:\n");
+                Console.WriteLine("1- Addition");
+                Console.WriteLine("2- Subtration");
+                Console.WriteLine("3- Multiplication");
+                Console.WriteLine("4- Division");
+                Console.WriteLine("5- View historic");
+                Console.WriteLine("6- Quit the game");
+                var gameSelected = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+                switch (gameSelected)
+                {
+                    case 1:
+                        MathOperations.Addition(); break;
+                         
+
+                    case 2:
+                        MathOperations.Subtraction(); break;
+
+                    case 3:
+                        MathOperations.Multiplication(); break;
+
+                    case 4:
+                        MathOperations.Division(); break;
+
+                    case 5:
+                        GetGames(); break;
+                    case 6:
+                        gameOn = false;
+                        Environment.Exit(1); break;
+                                     
+
+                    default: Console.WriteLine("Invalid Option"); break;
+
+                }
+            } while (gameOn);
+            
+            
+        }
+       static void GetGames()
+        {
+            Console.Clear();
+            Console.WriteLine("Games History:");
+            foreach (var game in games)
+            {
+                Console.WriteLine(game);
+                Console.WriteLine("Press any key to go back to the game");
+                Console.ReadLine();
+            }
+        }
+        
+        internal static void AddGame(string game)
+        {
+            games.Add(game);
         }
     }
+        
 }
